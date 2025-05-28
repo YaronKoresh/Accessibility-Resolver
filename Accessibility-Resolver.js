7d52c1b6e78d5bddcdf0beac5ea03bc08d25e6c5
Yaron Koresh <aharonkoresh1@gmail.com>
2025-05-28 16:36:50 +0300

    Delete Accessibility-Resolver.js

--- Accessibility-Resolver.js
+++
@@ -1,5 +0,0 @@
-const url = 'https://raw.githubusercontent.com/YaronKoresh/Accessibility-Resolver/refs/heads/main/0.html';
-const response = await fetch(url);
-const htmlContent = await response.text();
-const parser = new DOMParser();
-document.head.innerHTML += parser.parseFromString(htmlContent, 'text/html');
